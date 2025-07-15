a03b6b6019ce4b5372be3968e09f9e2ebb131d1a
biyane <kemaemaaa@gmail.com>
2025-07-15 17:56:29 +0500

    make robolectric and espresso appear it test folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,21 +47,21 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.junit.ktx)
 
 
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.ui.test.junit4)
+    testImplementation(platform(libs.androidx.compose.bom))
+    testImplementation(libs.androidx.junit)
+    testImplementation(libs.androidx.junit.ktx)
+    testImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.androidx.ui.test.junit4)
 
     testImplementation(libs.junit)
     // Optional -- Robolectric environment
